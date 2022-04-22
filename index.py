0eba8be102d946424bc82a8db119f063e7f850c9
mehmet <mehmeterim001@gmail.com>
2022-04-23 00:40:05 +0100

    first python

---
+++ index.py
@@ -0,0 +1,9 @@
+dict_by_dict = {'animal':'dog',
+			'planet': 'Neptun',
+			'number': 40,
+			'pi':3.14,
+			'is_good': True}
+
+print(dict_by_dict.items(), '\n')
+print(dict_by_dict.keys(), '\n')
+print(dict_by_dict.values(), '\n')
